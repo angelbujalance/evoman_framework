3b040657fbf61e4be27c5e5c4f9656a1a701f6d3
ChimeneBlokesch <chimene.blokesch2002@gmail.com>
2024-09-22 14:41:31 +0200

    Add type hint cont in playercontroller for NEAT

--- neat_controller.py
+++ neat_controller.py
@@ -5,20 +5,17 @@ import neat
 class PlayerControllerNEAT(Controller):
     def __init__(self, _n_hidden):
         self.n_hidden = [_n_hidden]
-        self.net = None  # NOTE: I guess the net can be initialized here
 
     def set(self, genome, n_inputs):
         # TODO
         # in the demo controller, the biases and weights were set here
-        # maybe set self.net here?
-        pass
+        ...
 
-    # NOTE: not sure how to use cont / if it's needed with NEAT
-    def control(self, params, cont=None):
+    def control(self, params, cont: neat.nn.FeedForwardNetwork = None):
         # Normalises the input using min-max scaling
         params = (params-min(params))/float((max(params)-min(params)))
 
-        output = self.net.activate(params)
+        output = cont.activate(params)
         left, right, jump, shoot, release = self.extract_net_output(output)
         return [left, right, jump, shoot, release]
 
